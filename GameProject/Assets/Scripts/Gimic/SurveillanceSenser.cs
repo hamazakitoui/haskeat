daabd1651a5ebe080a1de46aa88bb92b0a76ae19
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2023-02-08 12:54:29 +0900

    監視カメラスクリプト生成

---
+++ GameProject/Assets/Scripts/Gimic/SurveillanceSenser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> 監視カメラ </summary>
+public class SurveillanceSenser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
