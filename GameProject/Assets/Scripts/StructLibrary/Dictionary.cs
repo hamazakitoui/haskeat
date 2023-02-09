fe50535992e960aa624ed1669c4aa98dcc7ffe7e
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2023-02-09 10:36:32 +0900

    前回コミットされていなかったスクリプトを追加

---
+++ GameProject/Assets/Scripts/StructLibrary/Dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> 辞書 </summary>
+public struct Dictionary
+{
+    /// <summary> プレイヤータグ </summary>
+    public const string PLAYER_TAG = "Player";
+    /// <summary> エネミータグ </summary>
+    public const string ENEMY_TAG = "Enemy";
+    /// <summary> 赤スプレー </summary>
+    public const string RS_TAG = "redsplay";
+}
