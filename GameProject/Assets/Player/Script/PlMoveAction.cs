9e4d5533c3cfa7114e4df959eadc1bd622000bc0
hamazakitoui <nkc20206045@denpa.jp>
2023-02-09 10:44:00 +0900

    Playerの挙動（仮）

--- GameProject/Assets/Player/Script/PlMoveAction.cs
+++ GameProject/Assets/Player/Script/PlMoveAction.cs
@@ -5,33 +5,44 @@ using UnityEngine;
 public class PlMoveAction : MonoBehaviour
 {
     [SerializeField] float MoveSpeed;
+    [SerializeField] float RayLength;
+    [SerializeField] LayerMask NothitLayer;
+    [SerializeField] float HP;
+    [SerializeField] float invincibleTime;
     BoxCollider2D Collsion;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rigid2D;
     const int NowMove = 1;
     float InputX;
     float InputY;
     int nowcolor;
-    [SerializeField] GameObject[] coloreffect=new GameObject[4];
-    //enum Plstate
-    //{
-    //    stay,
-    //    move,
-    //    damege,
+    Vector3 dirctionkeap;
+    [SerializeField] GameObject[] coloreffect = new GameObject[4];
+    enum Plstate
+    {
+        none,
+        invincible,
 
-    //}
+    }
+    Plstate state = Plstate.none;
     // Start is called before the first frame update
     void Start()
     {
         //必要なコンポーネントを取得
         Collsion = GetComponent<BoxCollider2D>();
         rigid2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine("flash");
     }
 
     // Update is called once per frame
     void Update()
     {
-        InputX = Input.GetAxis("Horizontal");
-        InputY = Input.GetAxis("Vertical");
+
+        //移動を検知
+        InputX = Input.GetAxisRaw("Horizontal");
+        InputY = Input.GetAxisRaw("Vertical");
+        //スピード代入
         float speedX = 0;
         float speedY = 0;
         if (InputX == NowMove)
@@ -51,16 +62,16 @@ public class PlMoveAction : MonoBehaviour
         {
             speedY = -MoveSpeed;
         }
-        
-       
+
+        //移動
         rigid2D.velocity = new Vector2(speedX, speedY);
         //Move();
-       
+
     }
     private void Move()
     {
         float speedX = 0;
-        float speedY=0;
+        float speedY = 0;
         if (InputX == NowMove)
         {
             speedX = MoveSpeed;
@@ -85,7 +96,7 @@ public class PlMoveAction : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             nowcolor = 1;
-            
+
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
@@ -94,7 +105,8 @@ public class PlMoveAction : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             nowcolor = 3;
-        }else if (Input.GetKeyDown(KeyCode.Alpha4))
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             nowcolor = 4;
         }
@@ -102,11 +114,43 @@ public class PlMoveAction : MonoBehaviour
     }
     bool checkfront()
     {
-        Vector3 chkPos = transform.position;
+        //rayの初期位置
+        Vector3 RaystartPos = transform.position;
+        Vector3 direction = Vector3.up;
+        direction = new Vector3((int)InputX, (int)InputY);
+        if (direction != Vector3.zero)
+        {
+            dirctionkeap = direction;
+
+        }
+        else if (direction == Vector3.zero)
+        {
+            direction = dirctionkeap;
+        }
         bool result = false;
-        result|=Physics2D.Raycast(chkPos.x+InputX,)
-        return false;
-        
+        result |= Physics2D.Raycast(RaystartPos, direction, RayLength, NothitLayer);
+        Debug.DrawRay(RaystartPos, direction, Color.red);
+        return result;
+
+    }
+    IEnumerator flash()
+    {
+
+        Color co = spriteRenderer.color;
+        Color Before = co;
+        int count = 0;
+
+
+        Debug.Log("フェード中");
+        while (count < 10)
+        {
+            count++;
+
+            spriteRenderer.color = new Color(co.r, co.g, co.b,0);
+            yield return new WaitForSeconds(0.07f);
+            spriteRenderer.color = new Color(co.r,co.g,co.b,1);
+            yield return new WaitForSeconds(0.07f);
+        }
     }
 
 
