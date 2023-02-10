fcf4d209e24c16b646a73852cc463387e8f0043c
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2023-02-10 12:00:33 +0900

    美術品のスクリプトを追加

---
+++ GameProject/Assets/Scripts/Arts/Art.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> 芸術品 </summary>
+public class Art : MonoBehaviour
+{
+    // アニメ名
+    readonly string breakAnim = "Break";
+    [SerializeField] ArtType type; // 芸術品のタイプ
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary> 画像変更 </summary>
+    public void ChangeSprite()
+    {
+        Animator animator = GetComponent<Animator>(); // アニメーターコンポーネント取得
+        if (animator == null) return; // アニメーターコンポーネントがないなら
+        animator.SetTrigger(breakAnim); // アニメ変更
+    }
+    /// <summary> 芸術品の種類 </summary>
+    public ArtType GetArtType { get { return type; } }
+}
