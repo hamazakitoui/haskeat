0d9976787f0d36327fa76c16982cc27199549b6c
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2023-02-20 11:34:45 +0900

    タイトルマネージャー追加

---
+++ GameProject/Assets/Scripts/System/TitleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> タイトルマネージャー </summary>
+public class TitleManager : MonoBehaviour
+{
+    bool isLoad = false; // 開始フラグ
+    // キーコード
+    [SerializeField] KeyCode startKey;
+    [SerializeField] SceneObject GameScene; // ゲームシーン
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameStart(); // ゲーム開始
+    }
+    /// <summary> ゲーム開始 </summary>
+    void GameStart()
+    {
+        if (isLoad) return; // 既に読み込んでいるなら
+        // 開始キーが押されたら
+        if (Input.GetKeyDown(startKey))
+        {
+            FadeSceneManager.Instance.LoadScene(GameScene); // シーン移動
+            isLoad = true;
+        }
+    }
+}
