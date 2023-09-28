9786e8e6038896c999184e1c8f9991c0b498eefe
hamazakitoui <nkc20206045@denpa.jp>
2023-09-28 14:06:59 +0900

    タイトルの調整

--- GameProject/Assets/Scripts/System/TitleManager.cs
+++ GameProject/Assets/Scripts/System/TitleManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary> タイトルマネージャー </summary>
 public class TitleManager : MonoBehaviour
@@ -8,27 +9,46 @@ public class TitleManager : MonoBehaviour
     bool isLoad = false; // 開始フラグ
     // キーコード
     [SerializeField] KeyCode startKey;
+    [SerializeField] KeyCode tutorialKey;
     [SerializeField] GameObject PaintBall; // ペイントアニメーション
     [SerializeField] SpriteRenderer Renderer; // 絵
     [SerializeField] SceneObject GameScene; // ゲームシーン
+    [SerializeField] SceneObject tutorialScene; // ゲームシーン
     [SerializeField] Sprite[] pictures; // 絵の配列
     [SerializeField] bool IsTitle;
+    [SerializeField] Image SelectArrow;
     public bool Effectend;
+    const int maxselect = 2;
+    int nowselect = 0;
+    Vector3 Arrowstart;
+    int[] textpos = new int[3] { -223, -296, -382 };
+    enum selectname
+    {
+        start,
+        tutorial,
+        end
+    };
+    selectname selectnum = selectname.start;
     // Start is called before the first frame update
     void Start()
     {
+
         if (IsTitle)
         {
             PaintBall.SetActive(false); // ペイント非表示
             Renderer.sprite = pictures[Random.Range(0, pictures.Length)]; // 絵を乱数で設定
 
         }
+        Arrowstart = SelectArrow.GetComponent<RectTransform>().anchoredPosition;
+
+        Debug.Log(textpos[0]);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameselect();
         if (IsTitle)
         {
             GameStart(); // ゲーム開始
@@ -48,9 +68,26 @@ public class TitleManager : MonoBehaviour
         if (Input.GetKeyDown(startKey))
         {
             PaintBall.SetActive(true); // ペイント表示
-            FadeSceneManager.Instance.LoadScene(GameScene); // シーン移動
             isLoad = true;
+            switch (selectnum)
+            {
+                case selectname.start:
+                    FadeSceneManager.Instance.LoadScene(GameScene);
+                    break;
+                case selectname.tutorial:
+                    FadeSceneManager.Instance.LoadScene(tutorialScene);
+                    break;
+                case selectname.end:
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+                    break;
+            }
+
         }
+
     }
     void SceneLoad()
     {
@@ -60,5 +97,24 @@ public class TitleManager : MonoBehaviour
             FadeSceneManager.Instance.LoadScene(GameScene); // シーン移動
             isLoad = true;
         }
+    }
+    void gameselect()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if ((int)selectnum > 0)
+            {
+                selectnum--;
+
+            }
+                
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if ((int)selectnum < maxselect)
+                selectnum++;
+        }
+        SelectArrow.GetComponent<RectTransform>().anchoredPosition = new Vector3(Arrowstart.x,textpos[(int)selectnum]);
+        Debug.Log((int)selectnum);
     }
 }
---
+++ GameProject/Assets/Transition/Script/Transition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    [SerializeField] private Material _transitionIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BeginTransition());
+    }
+
+    // Update is called once per frame
+   
+    IEnumerator BeginTransition()
+    {
+        yield return Animate(_transitionIn, 1);
+    }
+
+    /// <summary>
+    /// time秒かけてトランジションを行う
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    IEnumerator Animate(Material material, float time)
+    {
+        GetComponent<Image>().material = material;
+        float current = 0;
+        while (current < time)
+        {
+            material.SetFloat("_Alpha", current / time);
+            yield return new WaitForEndOfFrame();
+            current += Time.deltaTime;
+        }
+        material.SetFloat("_Alpha", 1);
+    }
+}
