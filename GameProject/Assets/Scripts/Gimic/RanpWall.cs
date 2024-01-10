4220dddde155ab70df53564b2ff5c07b6e51280c
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2024-01-10 14:15:53 +0900

    ランプギミックのスクリプト追加

---
+++ GameProject/Assets/Scripts/Gimic/RanpWall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RanpWall : MonoBehaviour
+{
+    [Header("変更するタイル")] [SerializeField] Tilemap tilemap;
+    [Header("ライト")] [SerializeField] Light ranp;
+    /// <summary> ランプ変更 </summary>
+    /// <param name="value">変更後の値</param>
+    private void RanpChange(bool value)
+    {
+        ranp.gameObject.SetActive(!value); // ランプ表示変更
+        Color color = tilemap.color; // タイルの色
+        color.a = value ? 1.0f : 0.0f; // アルファ値を変更
+        tilemap.color = color;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        decoy d = collision.GetComponent<decoy>(); // デコイ検索
+        if (d != null)
+        {
+            RanpWall[] ranps = FindObjectsOfType<RanpWall>(); // 全てのランプ検索
+            // ランプ消灯 & 他のランプ点灯
+            foreach(var ranp in ranps)
+            {
+                ranp.RanpChange(ranp == this);
+            }
+        }
+    }
+}
