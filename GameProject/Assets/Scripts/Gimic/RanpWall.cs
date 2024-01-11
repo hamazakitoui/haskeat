100ddf703921d8d8fc3d4f6846e37438ecdaf29e
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2024-01-11 12:24:55 +0900

    ランプギミックの処理を変更

--- GameProject/Assets/Scripts/Gimic/RanpWall.cs
+++ GameProject/Assets/Scripts/Gimic/RanpWall.cs
@@ -5,16 +5,57 @@ using UnityEngine.Tilemaps;
 
 public class RanpWall : MonoBehaviour
 {
-    [Header("変更するタイル")] [SerializeField] Tilemap tilemap;
-    [Header("ライト")] [SerializeField] Light ranp;
-    /// <summary> ランプ変更 </summary>
-    /// <param name="value">変更後の値</param>
-    private void RanpChange(bool value)
+    // アニメーションステート名
+    readonly string stateAnim = "State";
+    Animator animator; // アニメーター
+    [Header("変更するタイル")] [SerializeField] Tilemap[] tilemap;
+    [Header("ライト")] [SerializeField] Light[] ranp;
+    // Start is called before the first frame update
+    void Start()
     {
-        ranp.gameObject.SetActive(!value); // ランプ表示変更
-        Color color = tilemap.color; // タイルの色
-        color.a = value ? 1.0f : 0.0f; // アルファ値を変更
-        tilemap.color = color;
+        animator = GetComponent<Animator>(); // アニメーター取得
+    }
+    /// <summary> ランプ変更 </summary>
+    /// <param name="value">変更後の値</param> <param name="colorkind">色</param>
+    private void RanpChange(decoy.Colorkind colorkind)
+    {
+        Color lightColor = Color.white; // 変更後の色
+        // インクの色で灯りを変更
+        switch (colorkind)
+        {
+            // 赤色
+            case decoy.Colorkind.red:
+                lightColor = Color.red;
+                break;
+            // 青色
+            case decoy.Colorkind.brue:
+                lightColor = Color.blue;
+                break;
+            // 黄色
+            case decoy.Colorkind.yellow:
+                lightColor = Color.yellow;
+                break;
+            default:
+                break;
+        }
+        // 灯りの色変更
+        if (lightColor != Color.white)
+        {
+            // 灯りの色変更
+            foreach(var r in ranp)
+            {
+                r.color = lightColor;
+            }
+        }
+        // アニメ変更
+        if (colorkind != decoy.Colorkind.purple) animator.SetInteger(stateAnim, (int)colorkind);
+        // タイル表示
+        for (int t = 0; t < tilemap.Length; t++)
+        {
+            Color color = tilemap[t].color; // タイルの色
+            color.a = t == (int)colorkind ? 1.0f : 0.0f; // アルファ値を変更
+            tilemap[t].color = color;
+        }
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,7 +66,7 @@ public class RanpWall : MonoBehaviour
             // ランプ消灯 & 他のランプ点灯
             foreach(var ranp in ranps)
             {
-                ranp.RanpChange(ranp == this);
+                ranp.RanpChange(d.state);
             }
         }
     }
