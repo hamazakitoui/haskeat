b9997953c43bb0ffe789c3d75be76c94426b37df
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2024-01-18 14:32:01 +0900

    デバッグ関数追加

--- GameProject/Assets/Scripts/Enemy/Policeman.cs
+++ GameProject/Assets/Scripts/Enemy/Policeman.cs
@@ -105,6 +105,7 @@ public class Policeman : EnemyBase, IEnemy
     {
         if (foundPlayer) return; // プレイヤーを見つけてたら無視
         animDelta += Time.deltaTime;
+        Debug.Log("de");
         if (animDelta > rotSpan)
         {
             int state = animator.GetInteger(stateAnim); // アニメ状態
